f3c449dd0d0605e2ded1f6ea928f213a5df01f54
RowidaElshenawy <rowidaalielshenawy@gmail.com>
2025-02-13 23:41:10 +0200

    lastt

--- src/components/All/All.jsx
+++ src/components/All/All.jsx
@@ -3,31 +3,36 @@ import React, { useEffect, useState } from 'react'
 import styles from './All.module.scss'
 import Ingredients from '../Ingredients/Ingredients'
 import { Link, useNavigate } from 'react-router-dom'
+import { CircleLoader } from 'react-spinners'
 export default function All() {
   let [meals, setmeals] = useState([])
+  let [loader,setLoader]= useState(false)
   async function getAllMeals() {
+    setLoader(true)
     let { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=')
     console.log(data);
     setmeals(data.meals)
+    setLoader(false)
   }
   useEffect(() => {
     getAllMeals()
   }, [])
   return (
     <>
-      <div className='  sm:flex sm:flex-wrap '>
+    {loader? <div className='h-screen flex justify-center items-center '><CircleLoader color='green' size={100}/></div>: <div className='  sm:flex sm:flex-wrap '>
         {meals.map(meal =>
           <div className= {`px-3 my-20   text-green-700 w-[90%] md:w-1/2 lg:w-[33.3333%] xl:w-[25%] ${styles.child}`}>
             <div className={`${styles.inner} bg-white rounded-3xl px-10 pb-5  flex flex-col items-center `}>
               <div className='relative bottom-11'>
                 <img className='rounded-full' src={meal.strMealThumb} alt="" />
               </div>
-              <h2 className=' text-black '>{meal.strMeal}</h2>
+              <h2 className=' text-black text-2xl'>{meal.strMeal}</h2>
               <p className='py-2 text-green-400 '><i class="fa-solid fa-earth-americas"></i> {meal.strArea}</p>
               <Link to={`/ingredients/${meal.idMeal}`} className={`${styles.bt} px-5 py-1 rounded-full bg-green-400 text-white  text-lg `} >View Recipe</Link>
             </div>
           </div>)}
-      </div>
+      </div> }
+      
     </>
   )
 }
--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 import IngredientsStyles from './Ingredients.module.scss'
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
+import { CircleLoader } from 'react-spinners';
 export default function Ingredients() {
   let {id} = useParams()
   console.log(id);
   const [mealDetails, setMealDetails] = useState([]);
   const [error , setErorr] = useState("")
+  let[loader,setLoader]=useState(false)
   async function getIngredients() {
     try{
+      setLoader(true)
       setErorr("")
       let { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
       console.log(data);
@@ -16,6 +19,7 @@ export default function Ingredients() {
         throw new Error("The ID you entered is incorrect,no meals available. Please enter another ID")
       }
       setMealDetails(data.meals[0])
+      setLoader(false)
     }
     catch(error){
       console.log(error.message);
@@ -29,7 +33,7 @@ export default function Ingredients() {
     <>
      {error ? <div className='h-screen'><div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 w-full" role="alert">
               {error}
-            </div> </div>  : <div className='lg:flex lg:flex-wrap lg:justify-between lg:items-start  '>
+            </div> </div> :loader? <div className='h-screen flex justify-center items-center '><CircleLoader color='green' size={100}/></div> : <div className='lg:flex lg:flex-wrap lg:justify-between lg:items-start  '>
         <div className="inner lg:w-[33%]">
           <h1>{mealDetails?.str}</h1>
           <h2 className=' text-black mb-5'>{mealDetails?.strMeal}</h2>
@@ -45,26 +49,26 @@ export default function Ingredients() {
         <div className='bg-white p-3 lg:w-[32%] mt-12'>
           <h3 className='border-b-2 border-gray-500 mb-2 p-1 text-3xl font-semibold pb-4'>Ingredients</h3>
           <ul>
-            <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
-              <h4 className=''>{mealDetails?.strIngredient1}</h4>
-              <span>{mealDetails?.strMeasure1}</span>
-            </li>
-            <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
-              <h4 className=''>{mealDetails?.strIngredient2}</h4>
-              <span>{mealDetails?.strMeasure2}</span>
-            </li>
-            <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
-              <h4 className=''>{mealDetails?.strIngredient3}</h4>
-              <span>{mealDetails?.strMeasure3}</span>
-            </li>
-            <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
-              <h4 className=''>{mealDetails?.strIngredient4}</h4>
-              <span>{mealDetails?.strMeasure4}</span>
-            </li>
-            <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
+            {mealDetails.strMeasure1? <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
+              <h4 className=''>{mealDetails.strIngredient1}</h4>
+              <span>{mealDetails.strMeasure1}</span>
+            </li>:""}
+            {mealDetails.strIngredient2?<li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
+              <h4 className=''>{mealDetails.strIngredient2}</h4>
+              <span>{mealDetails.strMeasure2}</span>
+            </li>:""} 
+            {mealDetails.strMeasure3? <li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
+              <h4 className=''>{mealDetails.strIngredient3}</h4>
+              <span>{mealDetails.strMeasure3}</span>
+            </li>:""}
+            {mealDetails.strMeasure4?<li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
+              <h4 className=''>{mealDetails.strIngredient4}</h4>
+              <span>{mealDetails.strMeasure4}</span>
+            </li>:""}
+            {mealDetails.strIngredient5?<li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
               <h4 className=''>{mealDetails.strIngredient5}</h4>
               <span>{mealDetails.strMeasure5}</span>
-            </li>
+            </li>:""}
             {mealDetails.strIngredient6 ?<li className='border-b-2 border-gray-500 mb-2 p-1 flex justify-between'>
               <h4 className=''>{mealDetails.strIngredient6}</h4>
               <span>{mealDetails.strMeasure6}</span>
